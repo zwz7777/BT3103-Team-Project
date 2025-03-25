008c2867599c8941d5c84d916706e5f1a8d42882
WWYYiXuan <e1156133@u.nus.edu>
2025-03-25 21:25:53 +0800

    Updated App.vue and Sidebar.vue with sidebar navigation and layout

---
+++ NUS-LostFound-project/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of the app.</p>
+    </div>
+  </template>
+<script>
+export default {
+ name:"Home" 
+}
+</script>
