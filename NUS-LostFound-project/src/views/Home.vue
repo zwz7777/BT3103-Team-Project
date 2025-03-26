9dd148ca2ba5464ab33a4898c0f832fb32b0883c
RaUzenL <laura.yuran.luo@gmail.com>
2025-03-26 14:29:28 +0800

    Add vue router config

--- NUS-LostFound-project/src/views/Home.vue
+++ NUS-LostFound-project/src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <!-- Website Header and Menu Bar -->
+    <!-- Website header and enter button -->
     <header>
       <div class="logo">
         <h1>NUS Lost & Found</h1>
       </div>
     </header>
+
+    <div class = "enter">
+      <h1>Report Lost Items</h1>
+      <p>Click here to report lost items.</p>
+      <router-link to="/ReportLost">Report Lost Item</router-link>
+    </div>
+
   </div>
 </template>
 
@@ -16,14 +23,25 @@ export default {
 </script>
 
 <style scoped>
-/* Styles for Header */
 header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #333;
+  padding: 10px 20px;
+  color: #0058b0;
+}
+
+.enter {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #0058b0;
   padding: 10px 20px;
   color: white;
+}
+
+.enter h1 {
+  margin: 0;
 }
 
 .logo h1 {
