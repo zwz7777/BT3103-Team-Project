7bbb435d2ace9dc3a19ded66626a9858dbb51e74
RaUzenL <laura.yuran.luo@gmail.com>
2025-03-26 15:29:43 +0800

    Added entrance page before login

---
+++ NUS-LostFound-project/src/views/Entrance.vue
@@ -0,0 +1,52 @@
+<template>
+    <!-- This is the entrance page before login -->
+    <div>
+      <header>
+        <div class="logo">
+          <h1>NUS Lost & Found</h1>
+        </div>
+      </header>
+  
+      <div class = "enter">
+        <h1>Login</h1>
+        <br>
+        <p>Click here to login</p>
+        <router-link to="/login">Login</router-link>
+      </div>
+  
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Home"
+  };
+  </script>
+  
+  <style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    color: #0058b0;
+  }
+  
+  .enter {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #0058b0;
+    padding: 10px 20px;
+    color: white;
+  }
+  
+  .enter h1 {
+    margin: 0;
+  }
+  
+  .logo h1 {
+    margin: 0;
+  }
+  </style>
+  
--- NUS-LostFound-project/src/views/Home.vue
+++ NUS-LostFound-project/src/views/Home.vue
@@ -1,18 +1,7 @@
 <template>
   <div>
-    <!-- Website header and enter button -->
-    <header>
-      <div class="logo">
-        <h1>NUS Lost & Found</h1>
-      </div>
-    </header>
-
-    <div class = "enter">
-      <h1>Report Lost Items</h1>
-      <p>Click here to report lost items.</p>
-      <router-link to="/ReportLost">Report Lost Item</router-link>
-    </div>
-
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the home page, with highlighted items</p>
   </div>
 </template>
 
@@ -21,30 +10,3 @@ export default {
   name: "Home"
 };
 </script>
-
-<style scoped>
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-  color: #0058b0;
-}
-
-.enter {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #0058b0;
-  padding: 10px 20px;
-  color: white;
-}
-
-.enter h1 {
-  margin: 0;
-}
-
-.logo h1 {
-  margin: 0;
-}
-</style>
