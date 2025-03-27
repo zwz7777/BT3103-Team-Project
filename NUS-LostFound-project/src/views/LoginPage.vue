39e10e86689f09a3436c2489e3073c3c017c1357
xinyu <1343966384@qq.com>
2025-03-27 14:25:46 +0800

    Add Login Page to router & Resolv 2 router index file issue

---
+++ NUS-LostFound-project/src/views/LoginPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1>Welcome to the Login Page</h1>
+      <p>This is the login page of the app.</p>
+    </div>
+  </template>
+<script>
+export default {
+ name:"LoginPage" 
+}
+</script>
